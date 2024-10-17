f148b1d139ae6176f4a53d5f2702f56ea2060f52
hoangnt <hoangnt.dev@gmail.com>
2024-10-17 14:46:40 +0900

    fix ESLint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'], // Bỏ qua thư mục build
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // Sử dụng ECMAScript 2020
+      globals: globals.browser, // Đặt môi trường globals cho trình duyệt
+      parser: tsParser, // Dùng parser cho TypeScript
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks, // Đăng ký plugin react-hooks
+      'react-refresh': reactRefresh, // Đăng ký plugin react-refresh
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules, // Sử dụng rule mặc định của react-hooks
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
-      ],
+      ], // Rule cho React Refresh
+      '@typescript-eslint/no-empty-object-type': 'off', // Tắt cảnh báo no-empty-object-type
     },
+    extends: [
+      js.configs.recommended, // Khuyến nghị từ @eslint/js
+      'plugin:react/recommended', // Khuyến nghị từ React
+      'plugin:@typescript-eslint/recommended', // Khuyến nghị từ TypeScript ESLint
+    ],
   },
-)
+];
