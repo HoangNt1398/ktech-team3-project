a390445511fcd93831ea38cd152952eae42ae9d9
hoangnt <hoangnt.dev@gmail.com>
2024-10-24 02:52:59 +0900

    update file vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteCommonjs()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // 크기 제한 값 설정 
+  },
+});
